f8abbcdab788a9f1b0519fac2a954b1803cf2e20
Rickard Jonsson <rickaard@gmail.com>
2023-12-05 22:25:28 +0100

    Day 1 - Part 2

--- lib/advent_of_code/day_one.ex
+++ lib/advent_of_code/day_one.ex
@@ -1,20 +1,51 @@
 defmodule AdventOfCode.DayOne do
-  def summed_calibration_values(input) when is_binary(input) do
+  @integer_word_to_string_integer %{
+    "one" => "o1e",
+    "two" => "t2o",
+    "three" => "th3ee",
+    "four" => "fo4r",
+    "five" => "fi5e",
+    "six" => "s6x",
+    "seven" => "se7en",
+    "eight" => "ei8ht",
+    "nine" => "ni9e"
+  }
+
+  @spec part_one(binary()) :: integer()
+  def part_one(input) when is_binary(input) do
     input
     |> String.split("\n", trim: true)
-    |> Enum.map(&calibration_value_for_line/1)
+    |> Enum.map(&calibration_value_for_line(&1, part: :one))
     |> Enum.sum()
   end
 
-  defp calibration_value_for_line(line) do
+  @spec part_two(binary()) :: integer()
+  def part_two(input) when is_binary(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&calibration_value_for_line(&1, part: :two))
+    |> Enum.sum()
+  end
+
+  defp calibration_value_for_line(line, part) do
     line
-    |> extract_string_integers()
+    |> extract_string_integers(part)
     |> then(&(List.first(&1) <> List.last(&1)))
     |> String.to_integer()
   end
 
-  defp extract_string_integers(string) do
+  defp extract_string_integers(string, part: :one) do
     string
+    |> String.split("", trim: true)
+    |> Enum.reject(&(Integer.parse(&1) == :error))
+  end
+
+  defp extract_string_integers(string, part: :two) do
+    @integer_word_to_string_integer
+    |> Map.keys()
+    |> Enum.reduce(string, fn word, transformed_string ->
+      String.replace(transformed_string, word, @integer_word_to_string_integer[word])
+    end)
     |> String.split("", trim: true)
     |> Enum.reject(&(Integer.parse(&1) == :error))
   end
@@ -22,5 +53,10 @@ end
 
 "lib/inputs/day_one.text"
 |> File.read!()
-|> AdventOfCode.DayOne.summed_calibration_values()
-|> IO.inspect(label: "Sum of all calibration values")
+|> AdventOfCode.DayOne.part_one()
+|> IO.inspect(label: "Sum of all calibration values - Part 1")
+
+"lib/inputs/day_one.text"
+|> File.read!()
+|> AdventOfCode.DayOne.part_two()
+|> IO.inspect(label: "Sum of all calibration values - Part 2")
--- test/advent_of_code/day_one_test.exs
+++ test/advent_of_code/day_one_test.exs
@@ -1,9 +1,9 @@
 defmodule AdventOfCode.DayOneTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
   alias AdventOfCode.DayOne
 
-  test "sums all calibration values for the passed input" do
+  test "sums all calibration values in the passed input for part one" do
     input = """
     1abc2
     pqr3stu8vwx
@@ -11,6 +11,100 @@ defmodule AdventOfCode.DayOneTest do
     treb7uchet
     """
 
-    assert 142 = DayOne.summed_calibration_values(input)
+    assert 142 = DayOne.part_one(input)
+  end
+
+  test "sums all calibration values in the passed input for part two" do
+    # 29 + 83 + 13 + 24 + 42 + 14 + 76
+    input_1 = """
+    two1nine
+    eightwothree
+    abcone2threexyz
+    xtwone3four
+    4nineeightseven2
+    zoneight234
+    7pqrstsixteen
+    """
+
+    # 77 + 42 + 34 + 29 + 67 + 11 + 22 + 85
+
+    input_2 = """
+    shhbgk74sevenrzknghzsk
+    4sevendeight2
+    bvjdfthreefive4pfxfvcxc6seven4zjkxdtcjr
+    2seven4three239kmjzfhbgxxmqv
+    sixfourd7
+    hxkkdnjkbmftmone1npmqbnx
+    rsxvqjbqx2bzprkml2
+    8twothree8dzqbrvdx5
+    """
+
+    # 57 + 41 + 63 + 84 + 62 + 47 + 46 + 93 + 49 + 94
+    input_3 = """
+    sxvd5t7
+    prkjfourhpdpxlvlnmz6onetl1
+    6one3cgxhd
+    nzeightwofourmz4
+    lfrnfdkgsixeight2hnlzksmjrsqjmvnmk
+    4jddtplseven
+    four7onefivenine16
+    93qd9svlnnskq8eighthree
+    bxsrknfournhqmqzjxmsix5nineb
+    9eightfour
+    """
+
+    # 53 + 99 + 58 + 67 + 39 + 49 + 26 + 48 + 24 + 71
+    input_4 = """
+    5573htvcvhkhb
+    998zfcsninejnjcgnkd
+    five4kdcncpsevenjftzlxmqlzxlfour8
+    pkxfxvhlsix5seven
+    3seveninezxz
+    4sixninesgctmxxxx
+    26lrlphfive4pcpdsnlntjsixtlgjrddmkx
+    hp49three48rkxmzv
+    rjrllvt2twotwo16four4
+    seven77dccfgqbf1
+    """
+
+    # 51 + 11 + 47 + 22
+    input_5 = """
+    five11eight1
+    1twoonepzrjzsdfourdbdrxg1
+    43347two7
+    2eightsevenonenine2
+    """
+
+    # 55 + 33 + 82 + 58 + 33
+    input_6 = """
+    five
+    threefivethree
+    eightwo
+    fiveeight3sppjtccnineeighteightnffgtlsdj
+    threethreetwothree
+    """
+
+    # 83 + 79 + 21 + 82 + 98 + 98 + 88 + 11 + 78 + 74
+    input_7 = """
+    eighthree
+    sevenine
+    twone
+    eightwo
+    nineight
+    nineeight
+    eeeight
+    oooneeone
+    sevenjxbbplfour488trzv
+    rcnvxqrsevenjttxd9fiverqzblpnrcjhbc4
+    """
+
+    assert 281 = DayOne.part_two(input_1)
+    assert 367 = DayOne.part_two(input_2)
+    assert 636 = DayOne.part_two(input_3)
+    assert 534 = DayOne.part_two(input_4)
+    assert 131 = DayOne.part_two(input_5)
+    assert 261 = DayOne.part_two(input_6)
+    assert 712 = DayOne.part_two(input_7)
+    assert 16 = DayOne.part_two("one1165bjcpkpsjfxlnmz6")
   end
 end
